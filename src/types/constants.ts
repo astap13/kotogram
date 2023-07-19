ad15afced63b0f382d1491989f0a08b3abae76c6
Kiryl Astapenka <astapenko.1304@gmail.com>
2023-07-19 17:28:31 +0200

    fix: change server

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,5 +1,5 @@
-const URL = 'http://127.0.0.1:3000';
-const URL2 = 'https://koto.onrender.com';
+const URL1 = 'http://127.0.0.1:3000';
+const URL = 'https://kotogram-server.onrender.com';
 const sessionId = '$2b$10$NhL.XLXwthdA4kACTPIJg.'
 
 export { URL, sessionId };
